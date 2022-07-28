523a438d6b70bfa3703be59442e976eed8db4b5e
Mauricio Alejandro Juica Arriagada <89223275+flauper2021@users.noreply.github.com>
2022-07-27 21:22:09 -0300

    primitive types
    created class that explains primitive types

---
+++ src/main/tipos-primitivos/java/TiposPrimitivos.java
@@ -0,0 +1,17 @@
+public class TiposPrimitivos {
+
+    int valorInteiroPrimitivo = 10; //tamanho: armazena 32 bits
+    double valorDoublePrimitivo = 10.58; // tamanho 64 bits (18.446.744.073.709.551.616)
+    float valorFloatPrimitivo = 10.00f; //tamanho 32 bits (2.147.483.647)
+    boolean valorBolleanoPrimitivo; // 1 bit (por padrão vem false)
+    char valorCharacterPrimitivo = 'a'; //16 bits (por padrão vem com valor '\u000')
+    long valorLongPrimitivo = 10L; // tamanho 64 bits
+    byte valorBytePrimitivo = 2; //tamanho: armazena ate 8 bits (255)
+    short valorShortPrimitivo = 22;//tamanho: armazena ate 16 bits (65.536)
+
+    // nomes de variaveis não podem começar com digito, ex: 0valor;
+    // não pode haver espaços em branco;
+    // não pode usar acentos ex til e ç;
+
+
+}
